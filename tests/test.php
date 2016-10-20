891a54f516469b3713455a993469f6747dbe3eb3
Stefan Harvey <stefan@devclever.co.uk>
2016-10-20 12:26:43 +0100

    Remove tests directory, and added .gitignore for vendor directory

--- tests/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__.'/../vendor/autoload.php'; // Autoload files using Composer
-use Central\CentralGeneral;
-echo CentralGeneral::inputField('hidden', 'test', 'test', 'test');
-?>
